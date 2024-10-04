4b70eac044c71597bad9ae79f6ce540cfa19bc70
i7 <i7@i7E-17>
2024-10-04 17:56:03 +0900

    1004 project

---
+++ src/main/java/com/shop/hermesPlus/HermesPlusApplication.java
@@ -0,0 +1,13 @@
+package com.shop.hermesPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermesPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/hermesPlus/HermesPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.hermesPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
