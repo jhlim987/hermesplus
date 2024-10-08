b33aeb90ae75ee04c56b293977b1d64cb3a8de14
621hjmit <superant621@gmail.com>
2024-10-08 15:43:39 +0900

    admin theme넣음.

---
+++ src/main/java/com/shop/hermesPlus/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.shop.hermesPlus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+	@GetMapping("/admin")
+	public String goAdminIndex() {
+		return "admin/index";
+	}
+	@GetMapping("/admin/**")
+	public void goAdmin() {}
+}
