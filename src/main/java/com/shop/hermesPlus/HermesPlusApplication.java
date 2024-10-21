a1380f1249a129dec887d737443d1a566c55127f
onestar12121 <onestar12121@naver.com>
2024-10-21 16:06:50 +0900

    MapperScan 어노테이션 추가

--- src/main/java/com/shop/hermesPlus/HermesPlusApplication.java
+++ src/main/java/com/shop/hermesPlus/HermesPlusApplication.java
@@ -1,9 +1,11 @@
 package com.shop.hermesPlus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan
 public class HermesPlusApplication {
 
 	public static void main(String[] args) {
