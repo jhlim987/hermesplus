558985e737af94f0694bc5f6583958cabf1a5247
Administrator <Administrator@i7E-31>
2024-10-24 12:29:16 +0900

    test

---
+++ src/main/java/com/shop/hermesPlus/common/util/HttpSessionUtil.java
@@ -0,0 +1,21 @@
+package com.shop.hermesPlus.common.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class HttpSessionUtil {
+
+	public static HttpSession getSession() {
+		ServletRequestAttributes sra =  (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
+		HttpServletRequest req = sra.getRequest();
+		HttpSession session = req.getSession();
+		return session;
+	}
+	
+	public static boolean isLogin() {
+		return getSession().getAttribute("user")!=null;
+	}
+}
